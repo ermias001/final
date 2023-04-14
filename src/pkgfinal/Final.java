d67b19c43477181902b0464bfbb9a8d40a8a2dd1
ermias001 <ermatekolla@gmail.com>
2023-04-14 14:43:34 +0300

    First Move TEST

--- src/pkgfinal/Final.java
+++ src/pkgfinal/Final.java
@@ -14,7 +14,6 @@ public class Final {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("TEST");
     }
-    
 }
